eaaa840523151f391a708e734e58d6fea24b33a0
ZhouHang <fireintheholezh@aliyun.com>
2018-10-06 23:16:17 +0800

    fix the tracking logic, add the direction judging

--- apple_detector.py
+++ apple_detector.py
@@ -8,15 +8,17 @@ sensor.reset()
 sensor.set_pixformat(sensor.RGB565)
 sensor.set_framesize(sensor.QVGA)
 sensor.skip_frames(time = 2000)
-
 clock = time.clock()
+img = sensor.snapshot()
+SIZE = (img.width(), img.height())
+CENTER = (img.width() / 2, img.height() / 2)
+AREA = img.size()
 
 green_led = LED(1)
 blue_led = LED(2)
 #broken1_led  = LED(3)
 #broken2_led    = LED(4)
 
-img = sensor.snapshot()
 appleRoi = ()
 
 #apple_th = (30, 75, 50, 70 ,0, 40)
@@ -28,7 +30,7 @@ apple_th = (30, 75, 40, 70 , 20, 60)
 
 #maxPixelCnt =
 
-def extendROI(Rect):
+def extendRoiWithBias(Rect, last_flag):
     v_x = Rect[0]
     v_y = Rect[1]
     r_width = Rect[2]
@@ -37,22 +39,61 @@ def extendROI(Rect):
     xCenter = v_x + r_width / 2
     yCenter = v_y + r_height / 2
 
+    # extend the area
     r_width = r_width * 1.2
     r_height = r_height *1.2
 
-    result = (int(xCenter - r_width / 2), int(yCenter - r_height / 2), int(r_width), int(r_height))
+    # add the bias
+    if last_flag[0] == 0:
+        x_b = 0
+    elif last_flag[0] == 1:
+        x_b = -10 # targe on right, roi turn left
+    else:
+        x_b = 10
+
+    if last_flag[1] == 0:
+        y_b = 0
+    elif last_flag[1] == 1:
+        y_b = -10 # target down, roi turn upwards
+    else:
+        y_b = 10
+
+    result = (int(xCenter - r_width/2 + x_b/2), int(yCenter - r_height/2 + y_b/2), int(r_width), int(r_height))
     return result
 
 def areaIsGood(Rect):
     r_w = Rect[2]
     r_h = Rect[3]
     #area = r_w * r_h
-
     if ((r_w < 80 and r_h < 105)or(r_w < 105 and r_h < 80)):
         return False
-
     return True
 
+def judgeDirection(Blob):
+    size = Blob.area()
+    min_b_x = 10 * (1 + size/AREA)
+    min_b_y = 10 * (1 + size/AREA)
+    obj_center_x = Blob.cx()
+    obj_center_y = Blob.cy()
+    bias_x = obj_center_x - CENTER[0]
+    bias_y = obj_center_y - CENTER[1]
+
+    if abs(bias_x) < min_b_x:
+        x_flag = 0 # still
+    elif bias_x > 0:
+        x_flag = 1 # target is on the right
+    else:
+        x_flag = 2 # target is on the left
+
+    if abs(bias_y) < min_b_y:
+        y_flag = 0 # still
+    elif bias_y > 0:
+        y_flag = 1 # target is down
+    else:
+        y_flag = 2 # target is up
+    return [x_flag, y_flag]
+
+last_flag = [0, 0] # 0 for x, 1 for y
 
 ## Main program
 while(True):
@@ -61,23 +102,17 @@ while(True):
     img = img
     print(clock.fps())
 
+
+
+
     green_led.off()
     blue_led.off()
-    # inrange select the color
-
-    # find edge
-    #grayImg = img.to_grayscale()
-    #edgImg = grayImg.find_edges(image.EDGE_CANNY, threshold=(0, 80))
-    #grayImg.laplacian(5)
-    #grayImg.binary([(2, 255)])
-    #grayImg.dilate(1)
-    #grayImg.midpoint(1)
 
 # find the blobs for apple
     # make sure the roi is not empty
     if appleRoi:
         # extend the roi, or the roi is likely to shrink on every loop
-        appleRoiEx = extendROI(appleRoi)
+        appleRoiEx = extendRoiWithBias(appleRoi, last_flag)
         objBoxs = img.find_blobs([apple_th], pixels_threhold = 200, roi = appleRoiEx, merge = True, margin = 0)
         print("roi found")
     else:
@@ -96,6 +131,7 @@ while(True):
         appleRoi = objBlob.rect()
         img.draw_rectangle(appleRoi, color = (255, 255, 0))
         print("obj found")
+        last_flag = judgeDirection(objBlob)
 
         green_led.on()
 
@@ -119,12 +155,3 @@ while(True):
 # when the target is close enough, shift into auto mode
     #if areaIsGood(objBlob.rect()):
         #unkown_led.toggle()
-
-
-
-
-
-
-
-
-
