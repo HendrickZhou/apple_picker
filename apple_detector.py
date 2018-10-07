7ff54273df86e80cf722932dc054312902910cf4
ZhouHang <fireintheholezh@aliyun.com>
2018-10-07 21:57:18 +0800

    fix communication logical error

--- apple_detector.py
+++ apple_detector.py
@@ -67,6 +67,9 @@ def extendRoiWithBias(Rect, last_flag):
     xCenter = v_x + r_width / 2
     yCenter = v_y + r_height / 2
 
+    const_x = 1
+    const_y = 1
+
     # extend the area
     r_width = r_width * 1.2
     r_height = r_height *1.2
@@ -75,16 +78,16 @@ def extendRoiWithBias(Rect, last_flag):
     if last_flag[0] == 0:
         x_b = 0
     elif last_flag[0] == 1:
-        x_b = -10 # targe on right, roi turn left
+        x_b = -const_x # targe on right, roi turn left
     else:
-        x_b = 10
+        x_b = const_x
 
     if last_flag[1] == 0:
         y_b = 0
     elif last_flag[1] == 1:
-        y_b = -10 # target down, roi turn upwards
+        y_b = -const_y # target down, roi turn upwards
     else:
-        y_b = 10
+        y_b = const_y
 
     result = (int(xCenter - r_width/2 + x_b/2), int(yCenter - r_height/2 + y_b/2), int(r_width), int(r_height))
     return borderCheck(result)
@@ -94,6 +97,7 @@ def areaIsGood(Rect):
     r_h = Rect[3]
     #area = r_w * r_h
     if ((r_w < 80 and r_h < 105)or(r_w < 105 and r_h < 80)):
+    #if ((r_w < 60 and r_h < 85)or(r_w < 85 and r_h < 60)):
         return False
     return True
 
@@ -133,9 +137,6 @@ onForwardPeriod = 0 # flag for the forwarding status
 #7 : 3
 #5,6 : 4
 
-#uartFlag:
-#0x00: stop; 0x01: forward; 0x02: back; 0x03: turnL;
-#0x04: turnR; 0x05: HeadUP; 0x06: HeadDOWN; 0x07: CUT;
 
 ## Main program
 while(True):
@@ -165,10 +166,10 @@ while(True):
                 # extend the roi, or the roi is likely to shrink on every loop
                 appleRoiEx = extendRoiWithBias(appleRoi, last_flag)
                 objBoxs = img.find_blobs([apple_th], pixels_threhold = 200, roi = appleRoiEx, merge = True, margin = 0)
-                print("roi found")
+                #print("roi found")
             else:
                 objBoxs = img.find_blobs([apple_th], pixels_threhold = 200, merge = True, margin = 0)
-                print("roi not founded!")
+                #print("roi not founded!")
 
             if objBoxs:
                 maxArea = 0
@@ -187,8 +188,15 @@ while(True):
                 # select the x flag
                 uartClass = 0
 
+                # when the target is close enough, shift into auto mode
+                if areaIsGood(appleRoi):
+                    #select the cut flag
+                    uartClass = 3
+
                 onDirAdjustPeriod = 1
                 green_led.on()
+
+
 
             else:
                 # lost the frame: extend the area, and backward
@@ -201,51 +209,64 @@ while(True):
                 print("obj not found!")
 
                 blue_led.toggle()
-                continue
+                #continue
 
-            print('\n')
+            #print('\n')
 
         # get the info of density and ratio of target
             #density = objBlob.density()
             #ratio = objBlob.w() / objBlob.h()
-            pixelCnt = objBlob.pixels()
-            area = objBlob.area()
+            #pixelCnt = objBlob.pixels()
+            #area = objBlob.area()
             #img.draw_string(appleRoi[0], appleRoi[1], str(density), color = (255, 255, 0))
-            print("pix: %d,  area: %d, width: %d, height: %d"%(pixelCnt, area, objBlob.w(), objBlob.h()))
+            #print("pix: %d,  area: %d, width: %d, height: %d"%(pixelCnt, area, objBlob.w(), objBlob.h()))
 
-        # when the target is close enough, shift into auto mode
-            if areaIsGood(objBlob.rect()):
-                #select the cut flag
-                uartClass = 3
+
 
     # send the flag
+
+#uartFlag:
+#0x00: stop; 0x01: forward; 0x02: back; 0x03: turnL;
+#0x04: turnR; 0x05: HeadUP; 0x06: HeadDOWN; 0x07: CUT;
 
     # left or right
     if uartClass == 0:
         if last_flag[0] == 0:
             uart.write(ustruct.pack("b", 0x00))
+            print("%s sent" %("stop"))
         if last_flag[0] == 1:
             uart.write(ustruct.pack("b", 0x04))
+            print("%s sent" %("right"))
         else:
             uart.write(ustruct.pack("b", 0x03))
+            print("%s sent" %("left"))
 
     # forward
     if uartClass == 1:
         uart.write(ustruct.pack("b", 0x01))
+        print("%s sent" %("forward"))
 
     # stop
     if uartClass == 2:
         uart.write(ustruct.pack("b", 0x00))
+        print("%s sent" %("stop"))
 
     # cut
     if uartClass == 3:
         uart.write(ustruct.pack("b", 0x07))
+        print("%s sent" %("cut"))
 
     # up or down
     if uartClass == 4:
         if last_flag[1] == 0:
             uart.write(ustruct.pack("b", 0x00))
+            print("%s sent" %("stop"))
         if last_flag[1] == 1:
             uart.write(ustruct.pack("b", 0x06))
+            print("%s sent" %("down"))
         else:
             uart.write(ustruct.pack("b", 0x05))
+            print("%s sent" %("up"))
+
+    time.sleep(30)
+    print('\n')
